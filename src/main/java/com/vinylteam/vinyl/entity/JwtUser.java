495ec2f158410cdc09fb596182483de7e4b036a4
edt77 <edt77@ukr.net>
2021-09-14 22:16:48 +0300

    SearchResultsController:  - reworked for the purpose of simplification;

--- src/main/java/com/vinylteam/vinyl/entity/JwtUser.java
+++ src/main/java/com/vinylteam/vinyl/entity/JwtUser.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Setter
 public class JwtUser implements UserDetails {
 
+    private Long id;
     private String username;
     private String password;
     private boolean enabled;
